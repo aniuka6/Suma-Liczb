aa378582b54eb4ae790d63309fdf6d0180263f88
aniuka6 <ania.tokarzewska@wp.pl>
2023-01-15 16:50:08 +0100

    first commit

---
+++ src/iloscLiczb.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class iloscLiczb {
+    public static void main(String[] args) {
+        System.out.println("Wprowadź ilość liczb jaką chcesz ze sobą zsumować");
+        Scanner sc = new Scanner(System.in);
+        int numbers =sc.nextInt();
+
+        System.out.println("Podaj liczby które chcesz ze sobą zsumować");
+
+        int suma =0;
+
+        for (int i=0; i < numbers; i++ ){
+
+            suma = suma + sc.nextInt();
+        }
+        System.out.println("Suma wszystkich wpisanych liczb to: " +suma);
+        sc.close();
+    }
+}
